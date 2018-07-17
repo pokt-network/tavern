b599d0359106fc51a621a52212d7508b7c6760e1
Luis de Leon <luyzdeleon@gmail.com>
2018-07-17 00:14:29 -0400

    Added rinkeby deployment configuration

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,12 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*'
+    },
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 4,
+      gas: 7003700
     }
   }
 };
